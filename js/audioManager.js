e5016a8cdd2f1af430022d0892b47a2557b3c6af
mattixpet <matthias01123@gmail.com>
2014-11-11 22:33:48 +0000

    audiomanager og bg music

---
+++ js/audioManager.js
@@ -0,0 +1,36 @@
+/*
+
+entityManager.js
+
+A module which handles arbitrary entity-management for "Asteroids"
+
+
+We create this module as a single global object, and initialise it
+with suitable 'data' and 'methods'.
+
+"Private" properties are denoted by an underscore prefix convention.
+
+*/
+
+
+"use strict";
+
+
+// Tell jslint not to complain about my use of underscore prefixes (nomen),
+// my flattening of some indentation (white), or my use of incr/decr ops 
+// (plusplus).
+//
+/*jslint nomen: true, white: true, plusplus: true*/
+
+
+var audioManager = {
+
+play : function(path, vol) {
+    if (vol===undefined) vol = 1;
+    var audio = new Audio(path);
+    audio.volume = vol;
+    audio.play();
+}
+
+};
+
