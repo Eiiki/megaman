4e2e846ee41e0197b59f86ff15297733dd22f0c0
unknown <morstius@gmail.com>
2014-11-19 14:58:32 +0000

    added more enemies

--- js/SpawnManager.js
+++ js/SpawnManager.js
@@ -10,11 +10,17 @@
 var Spawner = {
 //[lvl1 dada, lvl1 snake, lvl1 snake, lvl1 snake]
 _possibleEnemies : [[],[
-					{type: 'dada',cx:700, cy:3520,alive:false,canSpawnAgain:true,velX:0,velY:-0.5},
+					{type: 'dada',cx:590, cy:3620,alive:false,canSpawnAgain:true,velX:0,velY:-0.5},
 					{type: 'petiteSnakey',cx:648, cy:3560,alive:false,canSpawnAgain:true},
+					{type: 'dada',cx:900, cy:3750,alive:false,canSpawnAgain:true,velX:0,velY:-0.5},
 					{type: 'petiteSnakey',cx:1065, cy:3688,alive:false,canSpawnAgain:true},
-					{type: 'petiteSnakey',cx:1353, cy:3624,alive:false,canSpawnAgain:true}],
-					[],[],[],[],[],[],[]
+					{type: 'petiteSnakey',cx:1353, cy:3624,alive:false,canSpawnAgain:true},
+					{type: 'dada',cx:1520, cy:3580,alive:false,canSpawnAgain:true,velX:0,velY:-0.5},],
+					[
+					{type: 'petiteSnakey',cx:1832, cy:3208,alive:false,canSpawnAgain:true,isFlipped:true},
+					{type: 'petiteSnakey',cx:1768, cy:3144,alive:false,canSpawnAgain:true,isFlipped:true},
+					{type: 'petiteSnakey',cx:1704, cy:3080,alive:false,canSpawnAgain:true,isFlipped:true}],
+					[],[],[],[],[],[]
 					],
 
 update : function(du) {
@@ -23,7 +29,7 @@ update : function(du) {
 		var posEn = this._possibleEnemies[mp][i];
 		if(posEn.alive === false){
 			if(posEn.canSpawnAgain === false && 
-				(global.camX + 510 < posEn.cx || global.camX > posEn.cx)){
+				(global.camX+610 < posEn.cx || global.camX+100 > posEn.cx)){
 				posEn.canSpawnAgain = true;
 			}
 		}
@@ -45,6 +51,7 @@ spawnEnemy : function(mapPart,index){
 	            spawncy : posEn.cy,
 	            velX : posEn.velX,
 	            velY : posEn.velY,
+	            isFlipped : posEn.isFlipped
 	            });
 	            posEn.alive=true;
 	            posEn.canSpawnAgain = false;
