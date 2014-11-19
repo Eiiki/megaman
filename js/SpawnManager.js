7a5861c1699f0a61410b342f9ef87fc5fab967b1
unknown <morstius@gmail.com>
2014-11-19 13:33:31 +0000

    snakes generalized

--- js/SpawnManager.js
+++ js/SpawnManager.js
@@ -8,56 +8,28 @@
 "use strict";
 
 var Spawner = {
-//[lvl1 dada]
-_possibleEnemies : [{type: 'dada',cx:700, cy:3520,alive:false,canSpawnAgain:true,velX:0,velY:-0.5}],
-
-// special handling stuff for the petiteSnakeys
-_petiteSnakeys : [],
-_prevX : 0,
-
-spawnPetiteSnakeys : function(prevX) {
-    var xLeft = global.camX;
-    var xRight = xLeft + g_canvas.width;
-    var coords = global.petiteSnakeysCoords;
-    //this is the halfwidth of the petiteSnakeySprite
-    // will be used to adjust spawning visuals
-    var hw = 30;
-    for (var i = 1; i < coords.length; i++) {
-	    // left and right edges of the petiteSnakey sprite
-	    var leftEdge = coords[i].cx
-	    var rightEdge = coords[i].cx
-
-	    if (xRight > leftEdge && xLeft < leftEdge && !this._petiteSnakeys[i]) {
-	    	coords[i].isFlipped = prevX > leftEdge ? true : false;
-	    	
-	    	this._petiteSnakeys[i] = entityManager.generateEnemy('petiteSnakey',
-	    		coords[i]
-	    	)
-	    }
-	   	if ((xLeft > leftEdge || xRight < leftEdge) && this._petiteSnakeys[i]) {
-	    	this._petiteSnakeys[i].kill();
-	    	delete this._petiteSnakeys[i];
-	    }
-	}
-	return xLeft;
-},
+//[lvl1 dada, lvl1 snake, lvl1 snake, lvl1 snake]
+_possibleEnemies : [{type: 'dada',cx:700, cy:3520,alive:false,canSpawnAgain:true,velX:0,velY:-0.5},
+					{type: 'petiteSnakey',cx:648, cy:3560,alive:false,canSpawnAgain:true,velX:0,velY:0},
+					{type: 'petiteSnakey',cx:1065, cy:3688,alive:false,canSpawnAgain:true,velX:0,velY:0},
+					{type: 'petiteSnakey',cx:1353, cy:3624,alive:false,canSpawnAgain:true,velX:0,velY:0}],
 
 update : function(du) {
 	if(global.mapPart === 1){
-		if(this._possibleEnemies[0].alive === false){
-			if(this._possibleEnemies[0].canSpawnAgain === false && 
-				(global.camX + 510 < this._possibleEnemies[0].cx || global.camX > this._possibleEnemies[0].cx)){
-				this._possibleEnemies[0].canSpawnAgain = true;
+		for(var i = 0; i < this._possibleEnemies.length; i++){
+			if(this._possibleEnemies[i].alive === false){
+				if(this._possibleEnemies[i].canSpawnAgain === false && 
+					(global.camX + 510 < this._possibleEnemies[i].cx || global.camX > this._possibleEnemies[i].cx)){
+					this._possibleEnemies[i].canSpawnAgain = true;
+				}
 			}
+			if(global.camX + 510 > this._possibleEnemies[i].cx && global.camX < this._possibleEnemies[i].cx){
+		        if(this._possibleEnemies[i].alive === false
+		        	&& this._possibleEnemies[i].canSpawnAgain === true){
+		            this.spawnEnemy(i);
+		        }
+		    }
 		}
-		if(global.camX + 510 > this._possibleEnemies[0].cx && global.camX < this._possibleEnemies[0].cx){
-	        if(this._possibleEnemies[0].alive === false
-	        	&& this._possibleEnemies[0].canSpawnAgain === true){
-	            this.spawnEnemy(0);
-	        }
-	    }
-	    // spawn petiteSnakeys for map part 1
-	    this._prevX = this.spawnPetiteSnakeys(this._prevX);
 	}
 },
 
