ca450d5c374ae677b71e53a56e260194fe4ef11c
unknown <morstius@gmail.com>
2014-11-19 13:56:18 +0000

    cleaning spawnManager

--- js/SpawnManager.js
+++ js/SpawnManager.js
@@ -9,48 +9,53 @@
 
 var Spawner = {
 //[lvl1 dada, lvl1 snake, lvl1 snake, lvl1 snake]
-_possibleEnemies : [{type: 'dada',cx:700, cy:3520,alive:false,canSpawnAgain:true,velX:0,velY:-0.5},
-					{type: 'petiteSnakey',cx:648, cy:3560,alive:false,canSpawnAgain:true,velX:0,velY:0},
-					{type: 'petiteSnakey',cx:1065, cy:3688,alive:false,canSpawnAgain:true,velX:0,velY:0},
-					{type: 'petiteSnakey',cx:1353, cy:3624,alive:false,canSpawnAgain:true,velX:0,velY:0}],
+_possibleEnemies : [[],[
+					{type: 'dada',cx:700, cy:3520,alive:false,canSpawnAgain:true,velX:0,velY:-0.5},
+					{type: 'petiteSnakey',cx:648, cy:3560,alive:false,canSpawnAgain:true},
+					{type: 'petiteSnakey',cx:1065, cy:3688,alive:false,canSpawnAgain:true},
+					{type: 'petiteSnakey',cx:1353, cy:3624,alive:false,canSpawnAgain:true}]
+					],
 
 update : function(du) {
-	if(global.mapPart === 1){
-		for(var i = 0; i < this._possibleEnemies.length; i++){
-			if(this._possibleEnemies[i].alive === false){
-				if(this._possibleEnemies[i].canSpawnAgain === false && 
-					(global.camX + 510 < this._possibleEnemies[i].cx || global.camX > this._possibleEnemies[i].cx)){
-					this._possibleEnemies[i].canSpawnAgain = true;
-				}
+	var mp = global.mapPart;
+	for(var i = 0; i < this._possibleEnemies.length; i++){
+		var posEn = this._possibleEnemies[mp][i];
+		if(posEn.alive === false){
+			if(posEn.canSpawnAgain === false && 
+				(global.camX + 510 < posEn.cx || global.camX > posEn.cx)){
+				posEn.canSpawnAgain = true;
 			}
-			if(global.camX + 510 > this._possibleEnemies[i].cx && global.camX < this._possibleEnemies[i].cx){
-		        if(this._possibleEnemies[i].alive === false
-		        	&& this._possibleEnemies[i].canSpawnAgain === true){
-		            this.spawnEnemy(i);
-		        }
-		    }
 		}
+		if(global.camX + 510 > posEn.cx && global.camX < posEn.cx){
+	        if(posEn.alive === false
+	        	&& posEn.canSpawnAgain === true){
+	            this.spawnEnemy(mp,i);
+	        }
+	    }
 	}
 },
 
-spawnEnemy : function(index){
-	entityManager.generateEnemy(this._possibleEnemies[index].type, {
-	            cx : this._possibleEnemies[index].cx,
-	            cy : this._possibleEnemies[index].cy,
-	            spawncx : this._possibleEnemies[index].cx,
-	            spawncy : this._possibleEnemies[index].cy,
-	            velX : this._possibleEnemies[index].velX,
-	            velY : this._possibleEnemies[index].velY,
+spawnEnemy : function(mapPart,index){
+	var posEn = this._possibleEnemies[mapPart][index];
+	entityManager.generateEnemy(posEn.type, {
+	            cx : posEn.cx,
+	            cy : posEn.cy,
+	            spawncx : posEn.cx,
+	            spawncy : posEn.cy,
+	            velX : posEn.velX,
+	            velY : posEn.velY,
 	            });
-	            this._possibleEnemies[index].alive=true;
-	            this._possibleEnemies[index].canSpawnAgain = false;
+	            posEn.alive=true;
+	            posEn.canSpawnAgain = false;
 },
 
 death : function (deadPos){
+	var mp = global.mapPart;
 	for(var i = 0; i < this._possibleEnemies.length; i++){
-		if(this._possibleEnemies[i].cx === deadPos[0]
-			&& this._possibleEnemies[i].cy === deadPos[1])
-			this._possibleEnemies[i].alive = false;
+		var posEn = this._possibleEnemies[mp][i];
+		if(posEn.cx === deadPos[0]
+			&& posEn.cy === deadPos[1])
+			posEn.alive = false;
 	}
 },
 
