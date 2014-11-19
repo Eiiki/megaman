42c283ad6fb78a2d292e5bf320cc32618fdd015b
unknown <morstius@gmail.com>
2014-11-19 15:04:54 +0000

    lvl 1 enemies

--- js/SpawnManager.js
+++ js/SpawnManager.js
@@ -15,7 +15,8 @@ _possibleEnemies : [[],[
 					{type: 'dada',cx:900, cy:3750,alive:false,canSpawnAgain:true,velX:0,velY:-0.5},
 					{type: 'petiteSnakey',cx:1065, cy:3688,alive:false,canSpawnAgain:true},
 					{type: 'petiteSnakey',cx:1353, cy:3624,alive:false,canSpawnAgain:true},
-					{type: 'dada',cx:1520, cy:3580,alive:false,canSpawnAgain:true,velX:0,velY:-0.5},],
+					{type: 'dada',cx:1520, cy:3580,alive:false,canSpawnAgain:true,velX:0,velY:-0.5},
+					{type: 'dada',cx:1884, cy:3750,alive:false,canSpawnAgain:true,velX:0,velY:-0.5}],
 					[
 					{type: 'petiteSnakey',cx:1832, cy:3208,alive:false,canSpawnAgain:true,isFlipped:true},
 					{type: 'petiteSnakey',cx:1768, cy:3144,alive:false,canSpawnAgain:true,isFlipped:true},
@@ -29,7 +30,7 @@ update : function(du) {
 		var posEn = this._possibleEnemies[mp][i];
 		if(posEn.alive === false){
 			if(posEn.canSpawnAgain === false && 
-				(global.camX+610 < posEn.cx || global.camX+100 > posEn.cx)){
+				(global.camX+610 < posEn.cx || global.camX-100 > posEn.cx)){
 				posEn.canSpawnAgain = true;
 			}
 		}
