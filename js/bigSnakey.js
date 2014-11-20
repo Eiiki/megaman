891f194e2c31eeae75096b6293f9d8cf4698c3d5
eiiki <eiiki0808@gmail.com>
2014-11-20 03:12:59 +0000

    looking good bigsnakey

--- js/bigSnakey.js
+++ js/bigSnakey.js
@@ -10,22 +10,7 @@ function bigSnakey(descr) {
     this.width = this.sprite.width * this._scale;
     this.height = this.sprite.height * this._scale;
     this.isFlipped = false;
-};
-
-bigSnakey.prototype = new Enemy();
-
-bigSnakey.prototype.health = 1;
-bigSnakey.prototype.type = 'bigsnakey';
-bigSnakey.prototype.timeSinceShot = 0;
-bigSnakey.prototype.timeForBrickUpdate = 0;
-bigSnakey.prototype.hasShot = false;
-
-bigSnakey.prototype.megamanBehind = function() {
-    return this.isFlipped ? global.megamanX < this.cx : global.megamanX > this.cx;
-};
-
-bigSnakey.prototype.gatherBricks = function(){
-    var bricks = {
+    this._bricks = {
                     neck: [
                         {x: Map.getXPosition(this.cx+this.width/2*this._scale + 1*32), y: Map.getYPosition(this.cy+this.height/2*this._scale - 1*32) + 16},
                         {x: Map.getXPosition(this.cx+this.width/2*this._scale + 0*32), y: Map.getYPosition(this.cy+this.height/2*this._scale - 1*32) + 16},
@@ -71,34 +56,63 @@ bigSnakey.prototype.gatherBricks = function(){
                         ]
                     ]
                 };
-    return bricks;
+};
+
+bigSnakey.prototype = new Enemy();
+
+bigSnakey.prototype.health = 1;
+bigSnakey.prototype.type = 'bigsnakey';
+bigSnakey.prototype.timeSinceShot = 0;
+bigSnakey.prototype.timeForBrickUpdate = 0;
+bigSnakey.prototype.hasShot = false;
+
+bigSnakey.prototype.megamanBehind = function() {
+    return this.isFlipped ? global.megamanX < this.cx : global.megamanX > this.cx;
 };
 
 bigSnakey.prototype._updateBrickInWave = function(brick){
+    var zeroBrick1 = 0;
+    var zeroBrick2 = 0;
     if(brick.y > brick.lastY && brick.y < brick.nextY){
+        //in middle going down
         brick.y += 32;
         brick.nextY -= 32;
         brick.lastY += 32;
+        zeroBrick1 = brick.y - 1*32;
+        zeroBrick1 = brick.y - 2*32;
     }else if(brick.y < brick.lastY && brick.y > brick.nextY){
+        //in middle going up
         brick.y -= 32;
         brick.nextY += 32;
         brick.lastY -= 32;
+        zeroBrick1 = brick.y + 1*32;
+        zeroBrick1 = brick.y + 2*32;
     }else if(brick.y > brick.lastY && brick.y > brick.nextY){
+        //Lowest Pos
         brick.y -= 32;
         brick.nextY -= 32;
         brick.lastY += 32;
+        zeroBrick1 = brick.y - 1*32;
+        zeroBrick1 = brick.y + 2*32;
     }else if(brick.y < brick.lastY && brick.y < brick.nextY){
+        //highest pos
         brick.y += 32;
         brick.nextY += 32;
         brick.lastY -= 32;
+        zeroBrick1 = brick.y - 1*32;
+        zeroBrick1 = brick.y + 2*32;
     }
 
     Map.changeTile(brick.x, brick.y, 5);
     Map.changeTile(brick.x, brick.y + 32, 1);
+    Map.changeTile(brick.x, brick.y - 32, 0);
+    Map.changeTile(brick.x, zeroBrick1, 0);
+    Map.changeTile(brick.x, zeroBrick2, 0);
+    return brick;
 };
 
 bigSnakey.prototype.drawNeck = function(value) {
-    var bricks = this.gatherBricks();
+    var bricks = this._bricks;
     var neck = bricks.neck;
     for(var n = 0; n < neck.length; n++){
         Map.changeTile(neck[n].x, neck[n].y, value);
@@ -110,13 +124,13 @@ bigSnakey.prototype.drawNeck = function(value) {
 };
 
 bigSnakey.prototype.updateWave = function(){
-    var bricks = this.gatherBricks();
+    var bricks = this._bricks;
     var wave = bricks.wave;
     var last_nextChanges = 0;
     for(var n = 0; n < wave.length; n++){
         for(var brickIdx in wave[n]){
             var brick = wave[n][brickIdx];
-            this._updateBrickInWave(brick);
+            this._bricks.wave[n][brickIdx] = this._updateBrickInWave(brick);
         }
     }
 };
