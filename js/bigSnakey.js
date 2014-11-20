9b6738769f7bdf7c39afa490cccd3ed6ff85f78b
mattixpet <matthias01123@gmail.com>
2014-11-20 20:16:41 +0000

    big snakey mappart og log

--- js/bigSnakey.js
+++ js/bigSnakey.js
@@ -116,8 +116,8 @@ bigSnakey.prototype._updateBrickInWave = function(brick){
     var megamanLM = global.megamanX - global.megamanWidth/2;
     var megamanRM = global.megamanX + global.megamanWidth/2;
     if(util.isBetween(megamanLM, brick.x - 16, brick.x + 16) || util.isBetween(megamanRM, brick.x - 16, brick.x + 16)){
-        if(global.mapPart === 3 || global.mapPart === 3)global.collisionUpdate.shouldUpdate = true;
-        console.log(global.mapPart);
+        if(global.mapPart === 3 || global.mapPart === 6)global.collisionUpdate.shouldUpdate = true;
+        //console.log(global.mapPart);
         if(global.collisionUpdate.cy === 0) {
             global.collisionUpdate.cy = global.megamanY;
             global.collisionUpdate.lastY = global.megamanY;
